f88d686b6f5f171e5e95e1f37b71925e8bbbc80a
Pedro Vitor Rodrigues <pedro.vitor@gestao.gov.br>
2023-10-11 12:27:16 -0300

    11/10

--- carrossel.js
+++ carrossel.js
@@ -1,34 +1,70 @@
-// carrossel.js
+const slides = document.querySelectorAll(".slide");
+const indicadores = document.querySelector(".indicadores");
+let slideIndex = 0;
+let intervalo;
+let mouseSobreCarrossel = false; // Adicionamos uma variável para rastrear o mouse
 
-const noticias = document.querySelectorAll(".noticia");
-let indiceNoticiaAtual = 0;
+function mostrarSlide(n) {
+    slides.forEach((slide) => {
+        slide.style.display = "none";
+    });
 
-function mostrarNoticia(indice) {
-    noticias.forEach((noticia, index) => {
-        noticia.style.display = "none";
-        if (index === indice) {
-            noticia.style.display = "block";
+    if (n < 0) {
+        slideIndex = slides.length - 1;
+    } else if (n >= slides.length) {
+        slideIndex = 0;
+    }
+
+    slides[slideIndex].style.display = "block";
+    criarIndicadores();
+    iniciarIntervalo();
+}
+
+function avancarSlide() {
+    slideIndex++;
+    mostrarSlide(slideIndex);
+}
+
+function iniciarIntervalo() {
+    clearInterval(intervalo);
+    if (!mouseSobreCarrossel) { // Verifica se o mouse não está sobre o carrossel
+        intervalo = setInterval(avancarSlide, 4000);
+    }
+}
+
+function criarIndicadores() {
+    indicadores.innerHTML = "";
+    slides.forEach((slide, index) => {
+        const indicador = document.createElement("div");
+        indicador.classList.add("indicador");
+        indicador.addEventListener("click", () => {
+            mostrarSlide(index);
+        });
+        indicadores.appendChild(indicador);
+    });
+    marcarIndicadorAtivo();
+}
+
+function marcarIndicadorAtivo() {
+    const indicadoresArray = Array.from(indicadores.children);
+    indicadoresArray.forEach((indicador, index) => {
+        if (index === slideIndex) {
+            indicador.classList.add("ativo");
+        } else {
+            indicador.classList.remove("ativo");
         }
     });
 }
 
-function proximaNoticia() {
-    indiceNoticiaAtual++;
-    if (indiceNoticiaAtual >= noticias.length) {
-        indiceNoticiaAtual = 0;
-    }
-    mostrarNoticia(indiceNoticiaAtual);
-}
+// Eventos de mouse
+document.querySelector(".carrossel").addEventListener("mouseenter", () => {
+    mouseSobreCarrossel = true; // O mouse está sobre o carrossel
+    clearInterval(intervalo); // Pausa o intervalo
+});
 
-function noticiaAnterior() {
-    indiceNoticiaAtual--;
-    if (indiceNoticiaAtual < 0) {
-        indiceNoticiaAtual = noticias.length - 1;
-    }
-    mostrarNoticia(indiceNoticiaAtual);
-}
+document.querySelector(".carrossel").addEventListener("mouseleave", () => {
+    mouseSobreCarrossel = false; // O mouse não está mais sobre o carrossel
+    iniciarIntervalo(); // Reinicia o intervalo
+});
 
-mostrarNoticia(indiceNoticiaAtual);
-
-document.getElementById("proximo").addEventListener("click", proximaNoticia);
-document.getElementById("anterior").addEventListener("click", noticiaAnterior);
+mostrarSlide(slideIndex);
